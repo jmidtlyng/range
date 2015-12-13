4c909f93c7d6d89d92ada839405bb121e3443d49
jmidtlyng <jacobmidtlyng@gmail.com>
2015-12-13 15:44:25 -0800

    hides each edit template on transition and included headers to hint users to location

--- client/templates/startup_buttons/startup_buttons.js
+++ client/templates/startup_buttons/startup_buttons.js
@@ -1,8 +1,10 @@
 Template.startupButtons.events({
     "click .setupBtn": function(){
+        Session.set("track", 0);
         Session.set("setup", 1);
     },
     "click .trackBtn": function(){
+        Session.set("setup", 0);
         Session.set("track", 1);
     }
 });
